93368292700cfcf542a2153d792885c883835c54
Theo Madikgetla <theothedeveloper@gmail.com>
2026-03-11 21:28:33 +0200

    feat: Add login page locators

---
+++ locators/loginLocators.ts
@@ -0,0 +1,7 @@
+// We use the 'export' keyboard as it allows us to access the objects outside a class
+export const LoginLocators =
+    {
+        usernameInput: "#user-name",
+        passwordInput: "#password",
+        loginButton: "#login-button"
+    }
