c59bf66ed8fe79b28758f44e1e6b09b1186ea979
Theo Madikgetla <theothedeveloper@gmail.com>
2026-04-08 23:42:30 +0200

    feat: implement methods and tests for adding multiple and specific products to cart

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -46,4 +46,34 @@ export class ProductPage{
         return names;
     }
 
+    async addAllProductsIntoCart(){
+        const productButtons = await this.page.locator(ProductPageLocators.productButtons);
+        const count = await productButtons.count();
+
+        console.log(productButtons);
+        for (let i = 0; i < count; i++){
+            await productButtons.nth(i).click();
+            console.log("Item: ",i,  productButtons.nth(i), await productButtons.nth(i).textContent());
+            await this.page.waitForTimeout(3000);
+        }
+    }
+
+    async getNumberOfItemInCart(){
+        return await this.page.locator(ProductPageLocators.shoppingCartIcon).textContent()
+    }
+
+    async addSpecificProductsToCart(productName){
+        const addProducts = this.page.locator(ProductPageLocators.productInventory);
+        const count = await addProducts.count();
+
+        for (let i = 0; i < count; i++){
+            const name  = await addProducts.nth(i).textContent();
+            if (name && productName.includes(name.trim()))
+            {
+                await this.page.locator(ProductPageLocators.productButtons).nth(i).click();
+                await this.page.waitForTimeout(3000);
+            }
+        }
+    }
+
 }
